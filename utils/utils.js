7cc3124035309d633bdbcd470f4a0805f296f04b
lord <marialindananono@gmail.com>
2020-12-31 12:56:51 +0300

    env variables

--- utils/utils.js
+++ utils/utils.js
@@ -1,19 +1,12 @@
 export const API_URL =
   process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
-export const MAGIC_PUBLIC_KEY =
-  process.env.NEXT_PUBLIC_MAGIC_PUBLIC_KEY || "pk_test_1CDC9FFE78D75ED8";
+export const MAGIC_PUBLIC_KEY = process.env.NEXT_PUBLIC_MAGIC_PUBLIC_KEY;
 
-export const STRIPE_PK =
-  process.env.NEXT_PUBLIC_STRIPE_PK ||
-  "pk_test_51HD4TXGZmvdMGd5u0B04fLH1vXDAp4a3sIqX1WeVSs59ZLgTICkw7ORLF17xzk3qkEdZ3Jm2s2xRU7CgMCMdQ0rb00wE0zKTga";
+export const STRIPE_PK = process.env.NEXT_PUBLIC_STRIPE_PK;
 
 export const fromImageToUrl = (image) => {
   if (!image) {
     return "/vercel.svg"; //Or default image here
-  }
-  if (image.url.indexOf("/") === 0) {
-    //It's a relative url, add API URL
-    return `${API_URL}${image.url}`;
   }
 
   return image.url;
